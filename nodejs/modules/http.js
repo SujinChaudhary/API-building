20bbe14f6544f6340b26e0c5a4b2f9e4cd5a3e0d
Sujin <sujin09818610719@gmail.com>
2026-08-25 11:47:52 +0545

    Building Server

--- nodejs/modules/http.js
+++ nodejs/modules/http.js
@@ -1,2 +1,14 @@
 import http from 'http';
-http.createServer();
+const server = http.createServer((request ,response)=>{
+   response.writeHead(200,{
+    "content-type" : "application/json",
+   });
+    response.end(JSON.stringify({status: 'ok'}));
+
+});
+
+server.listen(2599,()=>{
+    console.log("Server Running at Port 2599");
+
+});
+
